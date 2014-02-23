33de94eb9f4c857da1f949e318f77336d214dd85
zhulantian <zhulantian@wandoujia.com>
2014-02-23 21:27:23 +0800

    add slidingmenu lib.

--- kuaipan/src/org/sky/kuaipan/FileBrowserActivity.java
+++ kuaipan/src/org/sky/kuaipan/FileBrowserActivity.java
@@ -1,7 +1,14 @@
 package org.sky.kuaipan;
 
+import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class FileBrowserActivity extends SherlockActivity {
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
 
 }
